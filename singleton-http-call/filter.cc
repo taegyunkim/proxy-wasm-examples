3758bccb13ea88ef9e84da01ef59886aea5448c1
Taegyun Kim <k.taegyun@gmail.com>
2020-07-20 15:19:48 -0400

    Implement singleton-http-call.

--- singleton-http-call/filter.cc
+++ singleton-http-call/filter.cc
@@ -8,15 +8,35 @@ class SingletonRootContext : public RootContext {
 public:
   explicit SingletonRootContext(uint32_t id, StringView root_id)
       : RootContext(id, root_id) {}
+
+  bool onStart(size_t) override;
+
+  void onTick() override;
 };
+
+bool SingletonRootContext::onStart(size_t /*vm_configuration_size*/) {
+  logDebug("VM instantiated");
+  proxy_set_tick_period_milliseconds(5000);
+  return true;
+}
+
+void SingletonRootContext::onTick() {
+  auto callback = [](uint32_t headers, size_t body_size, uint32_t trailers) {
+    auto body = getBufferBytes(BufferType::HttpCallResponseBody, 0, body_size);
+    logInfo("Http Call Response: " + body->toString());
+  };
+
+  auto result = httpCall(
+      "wasm_upstream",
+      {{":method", "GET"}, {":path", "/auth"}, {":authority", "wasm_upstream"}},
+      "", {}, 5000, callback);
+}
 
 class SingletonContext : public Context {
 public:
   explicit SingletonContext(uint32_t id, RootContext *root)
       : Context(id, root),
         root_(static_cast<SingletonRootContext *>(static_cast<void *>(root))) {}
-
-  FilterHeadersStatus onRequestHeaders(uint32_t headers) override;
 
 private:
   SingletonRootContext *root_;
@@ -26,7 +46,3 @@ static RegisterContextFactory
     register_SingletonContext(CONTEXT_FACTORY(SingletonContext),
                               ROOT_FACTORY(SingletonRootContext),
                               "singleton-http-call");
-
-FilterHeadersStatus SingletonContext::onRequestHeaders(uint32_t) {
-  return FilterHeadersStatus::Continue;
-}
