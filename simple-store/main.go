07eb932177ea308d367df29e4b8c10a6dfba2983
Taegyun Kim <k.taegyun@gmail.com>
2020-07-21 13:15:09 -0400

    Simple store.

---
+++ simple-store/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+var gldata = make(map[string]string)
+
+func store(w http.ResponseWriter, req *http.Request) {
+	log.Println("Received store request.")
+	key := req.Header.Get("key")
+
+	if key == "" {
+		w.Write([]byte("Key not found"))
+		return
+	}
+
+	value := req.Header.Get("value")
+	if value == "" {
+		w.Write([]byte("Value not found"))
+		return
+	}
+
+	gldata[key] = value
+
+	w.Write([]byte("Stored " + key + " -> " + value))
+}
+
+func retrieve(w http.ResponseWriter, req *http.Request) {
+	log.Println("Received retrieve request.")
+	key := req.Header.Get("key")
+
+	if key == "" {
+		w.Write([]byte("Key not found"))
+		return
+	}
+
+	if value, ok := gldata[key]; ok {
+		w.Write([]byte(value))
+	} else {
+		w.Write([]byte("No data found for key: " + key))
+	}
+}
+
+func main() {
+	http.HandleFunc("/store", store)
+	http.HandleFunc("/retrieve", retrieve)
+	log.Println("Starting server...")
+	http.ListenAndServe(":8080", nil)
+}
