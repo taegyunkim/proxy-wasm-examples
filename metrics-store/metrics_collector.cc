f8f4a45f6f8f364b5282ff5e6443f21fee613b50
Taegyun Kim <k.taegyun@gmail.com>
2020-07-22 20:06:09 -0400

    Implement metrics collector.

--- metrics-store/metrics_collector.cc
+++ metrics-store/metrics_collector.cc
@@ -1,4 +1,5 @@
 // NOLINT(namespace-envoy)
+#include <optional>
 #include <string>
 #include <unordered_map>
 
@@ -17,10 +18,48 @@ public:
       : Context(id, root), root_(static_cast<MetricsCollectorRootContext *>(
                                static_cast<void *>(root))) {}
 
+  FilterStatus onDownstreamData(size_t, bool) override;
+  FilterStatus onUpstreamData(size_t, bool) override;
+  void onDownstreamConnectionClose(PeerType) override;
+  void onUpstreamConnectionClose(PeerType) override;
+
 private:
   TcpMetrics metrics_;
+  uint64_t time_;
+
   MetricsCollectorRootContext *root_;
 };
+
+FilterStatus MetricsCollectorContext::onDownstreamData(size_t data_size, bool) {
+  metrics_.set_data_downstream(metrics_.data_downstream() + data_size);
+  return FilterStatus::Continue;
+}
+
+FilterStatus MetricsCollectorContext::onUpstreamData(size_t data_size, bool) {
+  metrics_.set_data_upstream(metrics_.data_upstream() + data_size);
+  return FilterStatus::Continue;
+}
+
+void MetricsCollectorContext::onDownstreamConnectionClose(PeerType) {
+  metrics_.set_latency(getCurrentTimeNanoseconds() - time_);
+
+  uint32_t token = 0;
+  auto result = resolveSharedQueue("singleton", "q1", &token);
+  if (result != WasmResult::Ok) {
+    logDebug("Failed to resolve shared queue token: " + toString(result));
+    return;
+  }
+
+  std::string data = metrics_.SerializeAsString();
+  result = enqueueSharedQueue(token, data);
+  if (result != WasmResult::Ok) {
+    logDebug("Failed to enqueue data: " + toString(result));
+  }
+}
+
+void MetricsCollectorContext::onUpstreamConnectionClose(PeerType) {
+  time_ = getCurrentTimeNanoseconds();
+}
 
 static RegisterContextFactory
     register_MetricsCollectorContext(CONTEXT_FACTORY(MetricsCollectorContext),
