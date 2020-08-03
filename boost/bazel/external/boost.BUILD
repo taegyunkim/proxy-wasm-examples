8a4067ebaede03267400a3bea48b3f60a469addd
Taegyun Kim <k.taegyun@gmail.com>
2020-08-02 20:17:48 -0400

    Filter compiling with boost headers

---
+++ boost/bazel/external/boost.BUILD
@@ -0,0 +1,7 @@
+load("@filter_example//bazel/external:boost.bzl", "boost_library")
+
+boost_library(
+    name = "callable_traits",
+    deps = [
+    ],
+)
--- boost/filter.cc
+++ boost/filter.cc
@@ -1,13 +1,31 @@
 // NOLINT(namespace-envoy)
 #include <string>
+#include <tuple>
+#include <type_traits>
 #include <unordered_map>
 
+#include "boost/callable_traits.hpp"
 #include "proxy_wasm_intrinsics.h"
+
+namespace ct = boost::callable_traits;
+
+// This function template helps keep our example code neat
+template <typename A, typename B> void assert_same() {
+  static_assert(std::is_same<A, B>::value, "");
+}
+
+// foo is a function object
+struct foo {
+  void operator()(int, char, float) const {}
+};
 
 class HttpAuthRootContext : public RootContext {
 public:
   explicit HttpAuthRootContext(uint32_t id, StringView root_id)
-      : RootContext(id, root_id) {}
+      : RootContext(id, root_id) {
+    // Use args_t to retrieve a parameter list as a std::tuple:
+    assert_same<ct::args_t<foo>, std::tuple<int, char, float>>();
+  }
 };
 
 class HttpAuthContext : public Context {
@@ -21,6 +39,4 @@ private:
 };
 static RegisterContextFactory
     register_HttpAuthContext(CONTEXT_FACTORY(HttpAuthContext),
-                             ROOT_FACTORY(HttpAuthRootContext),
-                             "http_auth");
-
+                             ROOT_FACTORY(HttpAuthRootContext), "http_auth");
